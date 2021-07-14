7d20ff0e7eca6e0e6966b6537abb8374ec92a149
Mahdi Rostami <MahdiRostami@Outlook.de>
2021-07-14 19:23:25 +0300

    Create domain layer

---
+++ Domain/Entities/STI.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class STI : BaseEntity
+    {
+        public short IslemTur { get; set; }
+        public string EvrakNo { get; set; }
+        public int Tarih { get; set; }
+        public int MalID { get; set; }
+        public decimal Miktar { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal Tutar { get; set; }
+        public string Birim { get; set; }
+        public virtual STK Mal { get; set; }
+
+
+
+    }
+}
---
+++ Domain/Entities/STK.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class STK : BaseEntity
+    {
+        public string MalKodu { get; set; }
+        public string MalAdi { get; set; }
+       
+        public virtual ICollection<STI> Islemller { get; set; }
+
+    }
+}
